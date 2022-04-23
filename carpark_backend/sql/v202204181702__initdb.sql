d486ed28cb13fa1c7e1c2cf36b91434f4cee4666
Hong Hanh <nguyenhonghanh221020@gmail.com>
2022-04-23 16:33:31 +0700

    [update] update sql

--- carpark_backend/sql/v202204181702__initdb.sql
+++ carpark_backend/sql/v202204181702__initdb.sql
@@ -56,20 +56,22 @@ alter table tbl_vehicle
     add primary key (id);
 
 -- tbl_parking_ticket_infor
-create table if not exists tbl_vehicle
+create table if not exists tbl_parking_ticket_infor
 (
     id             int auto_increment,
-    id_user        int                not null,
-    type           varchar(20)        not null comment 'car, motocycle, bicycle',
-    lincense_plate varchar(20)        not null,
-    color          varchar(10)        not null,
-    vehicle_brand  varchar(50)        not null,
-    status         smallint default 1 not null comment 'active: 1, parking: 2, deleted: 0',
-    constraint tbl_vehicle_id_uindex
+    id_vehicle     int                not null,
+    id_carpark     int                not null,
+    start_time     time default '00:00:00',
+    end_time       time default '23:59:59',
+    price          int                not null,
+    status         smallint default 1 not null comment 'active: 1, deleted: 0',
+    constraint tbl_parking_ticket_infor_id_uindex
         unique (id),
-    constraint tbl_vehicle_tbl_user_fk
-        foreign key (id_user) references tbl_user (id)
+    constraint tbl_parking_ticket_infor_tbl_vehicle_fk
+        foreign key (id_vehicle) references tbl_vehicle (id),
+    constraint tbl_parking_ticket_infor_tbl_carpark_fk
+        foreign key (id_carpark) references tbl_carpark (id)
 );
 
-alter table tbl_vehicle
+alter table tbl_parking_ticket_infor
     add primary key (id);
